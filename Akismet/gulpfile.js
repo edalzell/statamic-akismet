c509798a54721d6d5c3b853fb3fadc6ada7e8ac5
Erin Dalzell <erin@thedalzells.org>
2017-01-05 17:21:51 -0800

    Add release build

--- Akismet/gulpfile.js
+++ Akismet/gulpfile.js
@@ -1,8 +1,16 @@
 let elixir = require('laravel-elixir');
 let glob = require('glob');
+let zip = require('gulp-zip');
 
 require('laravel-elixir-vue');
 
+
 elixir(function (mix) {
     mix.webpack(glob.sync('./resources/assets/js/components/*.{vue,js}'), './resources/assets/js/scripts.js');
+});
+
+gulp.task('release', function() {
+   return gulp.src(['../**/*', '!./node_modules/', '!./node_modules/**'])
+       .pipe(zip('akismet.zip'))
+       .pipe(gulp.dest('../'));
 });
