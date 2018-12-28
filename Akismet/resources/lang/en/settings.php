0a89510b4d41533e6c3dbfc3c321603c4648dce6
edalzell <Erin@thedalzells.org>
2018-12-27 18:14:16 -0800

    localization

---
+++ Akismet/resources/lang/en/settings.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'akismet_key' => 'Akismet Key',
+    'roles' => 'Roles',
+    'user_fields' => 'User Fields',
+    'user_check_registrations' => 'Check User Registrations?',
+    'user_first_name_field' => 'First Name Field',
+    'user_last_name_field' => 'Last Name Field',
+    'user_content_field' => 'Content Field',
+    'form_fields_section' => 'Form & Fields',
+];
